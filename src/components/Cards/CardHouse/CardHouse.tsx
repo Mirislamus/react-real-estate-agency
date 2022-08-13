745802f017e06fd7b567fafe96dde35f7582c759
mirislamus <mirislamus@gmail.com>
2022-08-13 23:34:49 +0500

    fix slider

--- src/components/Cards/CardHouse/CardHouse.tsx
+++ src/components/Cards/CardHouse/CardHouse.tsx
@@ -28,45 +28,95 @@ const CardHouse = () => {
       </div>
 
       <div className="house__desc">
-        <div className="house__detail">
-          <Swiper
-            spaceBetween={30}
-            effect={"fade"}
-            navigation={true}
-            pagination={{
-              clickable: true,
-            }}
+        <Swiper
+            slidesPerView={'auto'}
             modules={[EffectFade, Navigation, Pagination]}
-            className="mySwiper"
+            className="mySwiper2"
             nested={true}
-          >
-            <SwiperSlide>
-              <img src="https://langformula.ru/wp-content/uploads/2019/04/house.jpg" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-            </SwiperSlide>
-          </Swiper>
-          <div className="house__detail-items">
-            <h4 className="house__detail-name">Новый дом под ключ</h4>
-            <span className="house__detail-price">30 200 000 ₽</span>
-            <span className="house__detail-square">76.4 м</span>
-            <span className="house__detail-floor">2/22 эт.</span>
-            <div className="house__detail-district">ул. Раменки, д.21</div>
-            <button className="house__detail-btn">
-              <img src={phone} alt="phone" />
-              Показать телефон
-            </button>
-          </div>
-        </div>
+            navigation={true}
+            spaceBetween={30}
+        >
+          <SwiperSlide>
+            <div className="house__detail">
 
-        <div className="house__detail">
+              <Swiper
+                spaceBetween={30}
+                effect={"fade"}
+                navigation={true}
+                pagination={{
+                  clickable: true,
+                }}
+                modules={[EffectFade, Navigation, Pagination]}
+                className="mySwiper"
+                nested={true}
+              >
+                <SwiperSlide>
+                  <img src="https://langformula.ru/wp-content/uploads/2019/04/house.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+                </SwiperSlide>
+              </Swiper>
+              <div className="house__detail-items">
+                <h4 className="house__detail-name">Новый дом под ключ</h4>
+                <span className="house__detail-price">30 200 000 ₽</span>
+                <span className="house__detail-square">76.4 м</span>
+                <span className="house__detail-floor">2/22 эт.</span>
+                <div className="house__detail-district">ул. Раменки, д.21</div>
+                <button className="house__detail-btn">
+                  <img src={phone} alt="phone" />
+                  Показать телефон
+                </button>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="house__detail">
+            <Swiper
+              spaceBetween={30}
+              effect={"fade"}
+              navigation={true}
+              pagination={{
+                clickable: true,
+              }}
+              modules={[EffectFade, Navigation, Pagination]}
+              className="mySwiper"
+              nested={true}
+            >
+              <SwiperSlide>
+                <img src="https://images.adsttc.com/media/images/629f/3517/c372/5201/650f/1c7f/large_jpg/hyde-park-house-robeson-architects_1.jpg?1654601149" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="https://langformula.ru/wp-content/uploads/2019/04/house.jpg" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="https://www.pittsburghmagazine.com/content/uploads/2020/05/Front-Yard.jpg" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="https://pix10.agoda.net/hotelImages/9065853/-1/142d78192fda46d5b58e14c9d3f2fe51.jpg?ca=9&ce=1&s=1024x768" />
+              </SwiperSlide>
+            </Swiper>
+            <div className="house__detail-items">
+              <h4 className="house__detail-name">Новый дом под ключ</h4>
+              <span className="house__detail-price">30 200 000 ₽</span>
+              <span className="house__detail-square">76.4 м</span>
+              <span className="house__detail-floor">2/22 эт.</span>
+              <div className="house__detail-district">ул. Раменки, д.21</div>
+              <button className="house__detail-btn">
+                <img src={phone} alt="phone" />
+                Показать телефон
+              </button>
+            </div>
+          </div>
+          </SwiperSlide>
+          <SwiperSlide>
+          <div className="house__detail">
           <Swiper
             spaceBetween={30}
             effect={"fade"}
@@ -78,48 +128,10 @@ const CardHouse = () => {
             className="mySwiper"
             nested={true}
           >
-            <SwiperSlide>
-              <img src="https://images.adsttc.com/media/images/629f/3517/c372/5201/650f/1c7f/large_jpg/hyde-park-house-robeson-architects_1.jpg?1654601149" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src="https://langformula.ru/wp-content/uploads/2019/04/house.jpg" />
-            </SwiperSlide>
             <SwiperSlide>
               <img src="https://www.pittsburghmagazine.com/content/uploads/2020/05/Front-Yard.jpg" />
             </SwiperSlide>
             <SwiperSlide>
-              <img src="https://pix10.agoda.net/hotelImages/9065853/-1/142d78192fda46d5b58e14c9d3f2fe51.jpg?ca=9&ce=1&s=1024x768" />
-            </SwiperSlide>
-          </Swiper>
-          <div className="house__detail-items">
-            <h4 className="house__detail-name">Новый дом под ключ</h4>
-            <span className="house__detail-price">30 200 000 ₽</span>
-            <span className="house__detail-square">76.4 м</span>
-            <span className="house__detail-floor">2/22 эт.</span>
-            <div className="house__detail-district">ул. Раменки, д.21</div>
-            <button className="house__detail-btn">
-              <img src={phone} alt="phone" />
-              Показать телефон
-            </button>
-          </div>
-        </div>
-
-        <div className="house__detail">
-          <Swiper
-            spaceBetween={30}
-            effect={"fade"}
-            navigation={true}
-            pagination={{
-              clickable: true,
-            }}
-            modules={[EffectFade, Navigation, Pagination]}
-            className="mySwiper"
-            nested={true}
-          >
-            <SwiperSlide>
-              <img src="https://www.pittsburghmagazine.com/content/uploads/2020/05/Front-Yard.jpg" />
-            </SwiperSlide>
-            <SwiperSlide>
               <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
             </SwiperSlide>
             <SwiperSlide>
@@ -141,8 +153,10 @@ const CardHouse = () => {
             </button>
           </div>
         </div>
+          </SwiperSlide>
+          <SwiperSlide>
 
-        <div className="house__detail">
+          <div className="house__detail">
           <Swiper
             spaceBetween={30}
             effect={"fade"}
@@ -179,6 +193,9 @@ const CardHouse = () => {
             </button>
           </div>
         </div>
+          </SwiperSlide>
+        </Swiper>
+
       </div>
     </section>
   );
